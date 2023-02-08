dcec8a3ab4f81e83fbb22f47b4e30854ba57afd6
Elviiss <elvis22elmejor@gmail.com>
2023-02-08 13:51:03 +0100

    creando base de datos

---
+++ database/db.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS database_links;
+CREATE DATABASE database_links CHARSET utf8mb4;
+USE database_links;
+
+--USER TABLE
+CREATE TABLE users(
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(20) NOT NULL,
+  password VARCHAR(25) NOT NULL,
+  email VARCHAR(25) NOT NULL,
+  fullname VARCHAR(100) NOT NULL
+);
+
+--LINKS TABLE
+CREATE TABLE links(
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  url VARCHAR(255) NOT NULL,
+  description TEXT,
+  user_id INT UNSIGNED,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
+)
