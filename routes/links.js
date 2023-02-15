aa3aecf846ea3a97ce22ca80bd564c1fc5b7bfb0
Elviiss <elvis22elmejor@gmail.com>
2023-02-15 09:31:07 +0100

    añadiendo formulario

--- routes/links.js
+++ routes/links.js
@@ -9,4 +9,12 @@ router.get('/', async (req, res, next) => {
   //res.send
 });
 
+router.get('/add', (req, res) => {
+  res.render('links/add')
+})
+
+router.post('/add', (req, res) => {
+  res.send('Recibido')
+})
+
 module.exports = router;
