891132898eb538517a2bb4c4b960d265153e43e5
Elviiss <elvis22elmejor@gmail.com>
2023-02-19 20:46:02 +0100

    Arreglando pequeños fallos

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,29 +1,41 @@
 const express = require('express');
+const { authenticate } = require('passport');
 const router = express.Router();
 const passport = require('passport')
+const { isLoggedIn, isNotLogged }= require('../lib/auth')
 
 /* GET users listing. */
-router.get('/signup', (req, res, next) => {
+router.get('/signup',isNotLogged, (req, res, next) => {
   res.render('auth/signup');
 });
 
-router.post('/signup', passport.authenticate('local.signup', {
+router.post('/signup',isNotLogged, passport.authenticate('local.signup', {
   successRedirect: '/profile',
   failureRedirect: '/signup',
   failureFlash: true
 }))
 
-router.get('/signin', (req, res) => {
+router.get('/signin',isNotLogged, (req, res) => {
   res.render('auth/signin')
 })
 
-router.post('/signin', (req, res) => {
-  console.log(req.body)
-  res.send('Login correct')
+router.post('/signin',isNotLogged, (req, res, next) => {
+  passport.authenticate('local.signin', {
+    successRedirect: '/profile',
+    failureRedirect: '/signin',
+      failureFlash: true
+  })(req, res, next)
 })
 
-router.get('/profile', (req, res) => {
-  res.send('This is your profile')
+router.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile')
+})
+
+router.get('/logout',isLoggedIn, (req, res) => {  
+  req.logOut(function(err){
+    if (err) return next(err)
+  })  
+  res.redirect('/signin')
 })
 
 module.exports = router;
--- routes/links.js
+++ routes/links.js
@@ -1,51 +1,61 @@
 const express = require('express');
 const router = express.Router();
+
 const pool = require('../database')
 
+const { isLoggedIn } = require('../lib/auth')
+
 /* GET users listing. */
-router.get('/', async (req, res, next) => {
-  const [links] = await pool.query('SELECT * FROM links')
+router.get('/',isLoggedIn, async (req, res, next) => {
+  const [ links ] = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id])
   console.log(links)
-  res.render('links/list', {links})
+  res.render('links/list', { links })
+  
 });
 
-router.get('/add', (req, res) => {
+router.get('/add',isLoggedIn, (req, res) => {
   res.render('links/add')
 })
 
-router.post('/add', async (req, res) => {
+router.post('/add',isLoggedIn, async (req, res) => {
+
+  const { title, url, description } = req.body
+  const newLink = {
+    title,
+    url,
+    description,
+    user_id: req.user.id
+  }
+  await pool.query('INSERT INTO links SET ?', [newLink])
+  req.flash('success', 'Link saved successfully')
+  res.redirect('/links')
+})
+
+
+router.get('/delete/:id',isLoggedIn, async (req, res) => {
+  const { id } = req.params
+  await pool.query('DELETE FROM links WHERE id = ?', [id])
+  req.flash('success', 'Link removed successfully')
+  res.redirect('/links')
+}
+)
+router.get('/edit/:id',isLoggedIn, async (req, res) => {
+  const { id } = req.params
+  const [ link ] = await pool.query('SELECT * FROM links WHERE id = ?', [id])
+  
+  res.render('links/edit', {link:link[0]})
+})
+
+router.post('/edit/:id',isLoggedIn, async (req, res) => {
+  const { id } = req.params
   const { title, url, description} = req.body
   const newLink = {
     title,
     url,
     description
   }
-  await pool.query('INSERT INTO links SET ?', [newLink])
-  res.send('Recibido')
-})
-
-router.get('/delete/:id', async (req, res) => {
-  const {id} = req.params
-  await pool.query('DELETE FROM links WHERE id = ?', [id])
-  res.redirect('/links')
-})
-
-router.get('/edit/:id', async (req, res) => {
-  const {id} = req.params
-  const [link] = await pool.query('SELECT * FROM links WHERE id = ?', [id])
-  res.render('links/edit', {link:link[0]})
-})
-
-router.post('/edit/:id', async (req, res) => {
-  const {id} = req.params
-  const {title, url, description} = req.body
-  const newLink = {
-    title,
-    url,
-    description
-  }
   await pool.query('UPDATE links SET ? WHERE id = ?', [newLink, id])
-  req.flash('success', 'Link saves successfully')
+  req.flash('success', 'Link updated successfully')
   res.redirect('/links')
 })
 
